420fa0b08acc5c3a94deeb00a1d9362c40d4434e
marine <mjrmbernard@gmail.com>
2025-01-07 10:27:13 +0100

    C7 | Gestion d'etat

--- App.js
+++ App.js
@@ -10,6 +10,7 @@ export default function App() {
     { id: '1', content: 'Note1', icon: 'code' },
     { id: '2', content: 'Note2', icon: 'book' },
     { id: '3', content: 'Note3', icon: 'favorite' },
+    { id: '4', content: 'Note4', icon: 'code' },
   ]);
 
   return (
