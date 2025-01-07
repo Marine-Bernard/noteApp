f4db5e2fdf7a407123243a570483f51a603d37db
marine <mjrmbernard@gmail.com>
2025-01-07 09:28:44 +0100

    C4 | Etape 4

--- App.js
+++ App.js
@@ -1,9 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Header from './components/header';
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <Header title="NotesApp" />
       <h1>NotesApp</h1>
       <StatusBar style="auto" />
     </View>
---
+++ components/header.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const Header = ({ title }) => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>{title}</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    position: 'absolute',
+    width: '100%',
+    backgroundColor: '#f4511e',    
+    top: '0' ,       
+    justifyContent: 'center', 
+    alignItems: 'center',  
+    height: 60,
+  },
+  title: {
+    fontSize: 20,
+    color: '#fff',
+    fontWeight: 'bold',
+  },
+});
+
+export default Header;
