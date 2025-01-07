d7d3879eeae8f05d502df702a16d538cc2204eed
marine <mjrmbernard@gmail.com>
2025-01-07 10:02:37 +0100

    C6 | Style des éléments

--- App.js
+++ App.js
@@ -3,12 +3,13 @@ import { StyleSheet, Text, View } from 'react-native';
 import Header from './components/header';
 import { FlatList } from 'react-native-web';
 import { useState } from 'react';
+import { MaterialIcons } from '@expo/vector-icons';
 
 export default function App() {
   const [notes, setNotes] = useState([
-    { id: '1', content: 'Note1' },
-    { id: '2', content: 'Note2' },
-    { id: '3', content: 'Note3' },
+    { id: '1', content: 'Note1', icon: 'code' },
+    { id: '2', content: 'Note2', icon: 'book' },
+    { id: '3', content: 'Note3', icon: 'favorite' },
   ]);
 
   return (
@@ -20,6 +21,7 @@ export default function App() {
         keyExtractor={(item) => item.id} 
         renderItem={({ item }) => (
           <View style={styles.note}>
+            <MaterialIcons name={item.icon} size={24} color="#00796b" style={styles.icon} />
             <Text style={styles.noteText}>{item.content}</Text>
           </View>
         )}
@@ -30,6 +32,16 @@ export default function App() {
 }
 
 const styles = StyleSheet.create({
+  note: {
+    padding: 20,
+    borderBottomWidth: 1,
+    borderBottomColor: '#ccc',
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    marginVertical: 10,
+  },
   container: {
     flex: 1,
     backgroundColor: '#fff',
