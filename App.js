c9b4db49984c2fe222e396c95782f9e17059d60a
marine <mjrmbernard@gmail.com>
2025-01-07 09:22:07 +0100

    C3 | Etape 3

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello world</Text>
+      <h1>NotesApp</h1>
       <StatusBar style="auto" />
     </View>
   );
